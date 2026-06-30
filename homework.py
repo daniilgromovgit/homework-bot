5d8a6aa875cfcc76251374fd5808cd1a20fd0cfb
Daniil Gromov <grom0xff.mail@yandex.com>
2026-06-30 11:44:14 +0300

    Add bot functions

--- homework.py
+++ homework.py
@@ -1,10 +1,12 @@
 import os
 import logging
+import requests
 
 from dotenv import load_dotenv
 from telebot import TeleBot
-load_dotenv()
 
+
+load_dotenv()
 
 PRACTICUM_TOKEN = os.getenv("PRACTICUM_TOKEN")
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
@@ -26,7 +28,9 @@ logging.basicConfig(
     level=logging.INFO,
 )
 
-def check_tokens(): ...
+bot = TeleBot(token=TELEGRAM_TOKEN)
+bot.send_message(chat_id=TELEGRAM_CHAT_ID, text='Спасибо, что включили меня')
+def check_tokens():
     required_tokens = {
         'PRACTICUM_TOKEN': PRACTICUM_TOKEN,
         'TELEGRAM_TOKEN': TELEGRAM_TOKEN,
@@ -34,16 +38,28 @@ def check_tokens(): ...
     }
     missing = [name for name, token in required_tokens.items() if not token]
     for name in missing:
-        logging.critical(f'Отсутствует обязательная переменная окружения: {name}')
+        logging.critical(f'Отсутствует обязательная переменная окружения: {name}.')
     return not missing
 
-def send_message(bot, message): ...
+def send_message(bot, message) -> bool:
+    try:
+        bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message)
+    except Exception as e:
+        logging.error(f'Ошибка при отправке сообщения: {e}.')
+        return False
+    logging.debug(f'Сообщение успешно отправлено!')
+    return True
 
 
-def get_api_answer(timestamp): ...
+def get_api_answer(timestamp):# -> Any:
+    try:
+        homework_statuses: requests.Response = requests.get(ENDPOINT, headers=HEADERS, params={'from_date': timestamp})
+    except Exception as e:
+        logging.error(f'Эндпоинт недоступен: {e}.')
+    return homework_statuses.json()
 
 
-def check_response(response): ...
+def check_response(response):
 
 
 def parse_status(homework):
